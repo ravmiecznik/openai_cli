64befb5a18644ca8fac4077b8744101010500dfd
rafal <ravmiecznk@gmail.com>
2023-04-10 13:44:14 +0200

    typo

--- chatgpt.sh
+++ chatgpt.sh
@@ -3,8 +3,8 @@
 export OPENAI_API_KEY=$(cat .token | tr -d '\n')
 
 function chatg { 
-	prompt=\""$@\""
-	resp=$(openai api chat_completions.create -m gpt-3.5-turbo -g user "$prompt")
+	query=\""$@\""
+	resp=$(openai api chat_completions.create -m gpt-3.5-turbo -g user "$query")
 	fold -s -w $((2*$COLUMNS/3)) <<< $resp
 	echo
 }
