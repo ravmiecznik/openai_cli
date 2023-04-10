4589ed7f2561423c8cfb5d5fc23361921f74f840
rafal <ravmiecznk@gmail.com>
2023-04-10 13:15:36 +0200

    argument parser, can choose to save result

--- chatgpt.py
+++ chatgpt.py
@@ -2,6 +2,7 @@
 import sys
 import openai
 import datetime
+import argparse
 
 
 # Load your API key from an environment variable or secret management service
@@ -11,7 +12,7 @@ def main(question):
     with open('.token') as token:
         openai.api_key = token.read().strip()
 
-    response = openai.ChatCompletion.create(
+    resp_raw = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user",
@@ -19,23 +20,29 @@ def main(question):
              }
         ]
     )
-
-    resp_dict = response.to_dict()
-
-    now = datetime.datetime.now()
-    date_string = now.strftime("%Y-%m-%d_%H_%M_%S")
-
+    resp_dict = resp_raw.to_dict()
     resp_text = resp_dict['choices'][0]['message']['content']
-    print(resp_text)
-
-    with open(f'response{date_string}.txt', 'w') as resp:
-        resp.write(f"question: '{question}'\n")
-        resp.write("response:\n")
-        resp.write(resp_text)
-        resp.write('\n' + 30 * '-')
-        resp.write(str(response))
+    return resp_text, resp_raw
 
 
 if __name__ == "__main__":
-    q = ' '.join(sys.argv[1:])
-    main(q)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-r', '--save-resp', action='store_true', help='save detailed response')
+    parser.add_argument('query', nargs='*', help='chatgpt question')
+    args = parser.parse_args()
+    # print(args.query)
+    q = ' '.join(args.query)
+    resp_txt, resp_raw = main(q)
+
+    if args.save_resp:
+        now = datetime.datetime.now()
+        date_string = now.strftime("%Y-%m-%d_%H_%M_%S")
+        with open(f'response{date_string}.txt', 'w') as resp:
+            resp.write(f"question: '{q}'\n")
+            resp.write("response:\n")
+            resp.write(resp_txt)
+            resp.write('\n' + 30 * '-')
+            resp.write(str(resp_raw))
+
+    print(resp_txt)
