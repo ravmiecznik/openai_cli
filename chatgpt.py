31f0e7ba1a667ffba744231fc6b8451c662c169a
rafal <ravmiecznk@gmail.com>
2023-04-11 19:17:48 +0200

    stdin support, interactive mode implemented

--- chatgpt.py
+++ chatgpt.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 import os
 import sys
+import select
 
 import openai
 import datetime
@@ -33,16 +34,43 @@ def main(question):
 
 
 if __name__ == "__main__":
+
+    stdin_pending, _, _ = select.select([sys.stdin], [], [], 0.001)
+
+    if stdin_pending:
+        resp_text, resp_raw = main(sys.stdin.read())
+        print(resp_text)
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-r', '--save-resp', action='store_true', help='save detailed response')
     parser.add_argument('-t', '--get-token', action='store_true', help='retrieves token from .token file')
+    parser.add_argument('-i', '--interactive', action='store_true', help='interactive mode')
     parser.add_argument('query', nargs='*', help='chatgpt question')
     args = parser.parse_args()
 
     if args.get_token:
         print(get_token())
         sys.exit(0)
+
+    if args.interactive:
+        prompt = "?> "
+        print("---INTERACTIVE MODE---")
+        print("'CTRL-C or 'end conversation' to stop")
+        print(f"start with '^' to refer to previous question: {prompt}^ elaborate more")
+
+        question = input(prompt)
+        while question:
+            question_prev = question
+            resp = main(question)[0]
+            print(f"chatgpt: {resp}")
+            question = input(prompt)
+            if question.startswith('^'):
+                question = question[1:]
+                question = f"In regards to question: {question_prev}\nyou answered: {resp}\n{question}"
+                print(question)
+            elif question == 'end conversation':
+                break
 
     q = ' '.join(args.query)
     resp_txt, resp_raw = main(q)
