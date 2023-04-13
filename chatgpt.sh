690f1353985479a1b8324e0eadc343d6e5c3c9ae
rafal <ravmiecznk@gmail.com>
2023-04-13 10:09:48 +0200

    export env vars, can record a session, stores conversations in common location

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+artifacts_base_path=$VIRTUAL_ENV
+if [ -z $artifacts_base_path ] 
+then
+	echo Activate virtual env first ! >&2
+	return 1
+fi
+
+artifacts_base_path=$artifacts_base_path/conversations
+
+mkdir -p $artifacts_base_path
+mkdir -p $artifacts_base_path/responses
+
+export artifacts_base_path
+
+echo Convesration artifacts will be saved in: $artifacts_base_path
 
 export OPENAI_API_KEY=$(chatgpt --get-token)
 
@@ -13,11 +28,11 @@ function chatsh {
 # Access last conversation, to keep thread use ./chatgpt.py -r some question
 function last_conversation {
 	arg=$1
-	ls -tr responses/response_simple* >/dev/null 2>&1
+	ls -tr $artifacts_base_path/responses/response_simple* >/dev/null 2>&1
 	status=$?
 	if [ $status = 0 ]
 	then
-		last_resp=$(ls -tr responses/response_simple*| tail -n 1)
+		last_resp=$(ls -tr $artifacts_base_path/responses/response_simple*| tail -n 1)
 		if [[ $arg == "pop" ]]
 		then
 			rm $last_resp
@@ -27,11 +42,28 @@ function last_conversation {
 	fi
 }
 
+# Not used yet
 function find_conversation {
   keyword=$1
-  files_matching="$(grep "$keyword" -il responses/* | xargs -I {} head -c 40 {})"
+  files_matching="$(grep "$keyword" -il $artifacts_base_path/responses/* | xargs -I {} head -c 40 {})"
   echo $files_matching
 #  echo "$files_matching"
 }
 
+
+
+# Record terminal actions
+function record {
+        mkdir -p $artifacts_base_path/scripts
+        export record_file=$artifacts_base_path/scripts/script_$(date +"%Y-%m-%d_%H-%M-%S").scr
+        [ -f $record_file ] || script $record_file
+
+}
+
+function show_record {
+        cat $record_file
+}
+
+export -f record
+export -f show_record
 export -f chatsh
