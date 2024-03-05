fbfb0d1a9ab6b46d93cf1e0f1524f32dedc53b26
ravmiecznik <ravmiecznik@users.noreply.github.com>
2024-03-05 17:36:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ python3 -m venv $env &&
 ln -sf $PWD/chatgpt.py $PWD/$env/bin/chatgpt &&
 ln -sf $PWD/.token $PWD/$env/bin/.token &&
 source $env/bin/activate &&
-python -m pip install openai &&
+python -m pip install openai==0.28 &&
 pip install openai --upgrade 
